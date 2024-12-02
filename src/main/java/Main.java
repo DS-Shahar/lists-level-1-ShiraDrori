6f79e65b7121e4672c4327b3bc49ff36ea215731
ShiraDrori <shira.drori.sd@gmail.com>
2024-12-02 16:19:42 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,44 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+package tht;
+public class hjj {
+
+
+public static Node<Integer> exc1(int n, int x, int y) {	
+	Node <Integer> F = new Node <> ((int)(Math.random()*(y-x+1))+x);
+	Node <Integer> C = F;
+	Node <Integer> next;
+
+	for (int i=0; i<n; i++) {
+		next = new Node <> ((int)(Math.random()*(y-x+1))+x);
+		C.setNext(next);
+		C = next;
+
+	}
+	return F;
+
+
 }
+
+public static int exc2 (int x, Node <Integer> h) {
+	int count = 0;
+	Node <Integer> current = h;
+	
+	while (current != null) {
+		if (current.getValue() == x) {
+			count++;
+		}
+		current = current.getNext();
+	}
+	return count;
+}
+
+
+	
+	public static void main(String[] args) {
+		Node <Integer> Z = exc1(4,5,6);
+		System.out.println(Z);
+		System.out.println(exc2(5, Z));
+		
+
+	}
+
+} 
